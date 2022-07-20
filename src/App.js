54cdb36cdcf98047f91fca860a7b8c6362ac5991
SWATI SHARMA <99734340+Swatisharma78@users.noreply.github.com>
2022-07-21 02:01:01 +0530

    Day2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CurrentJobs from './Components/CurrentJobs';
+
+function App() {
+  return (
+    <div className="App">
+     <CurrentJobs/>
+    </div>
+  );
+}
+
+export default App;
